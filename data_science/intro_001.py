8ecab244c59ef0facfb792f5ffec01f13c8141f1
VENGATESHWARAN N <141017184+004vengatesh@users.noreply.github.com>
2023-07-31 15:26:40 +0530

    Update intro_001.py

--- data_science/intro_001.py
+++ data_science/intro_001.py
@@ -7,12 +7,16 @@ c = a+b
 print(c)
 
 
+
 def cs():
     print("CS Guys")
     c = a - b
     print(c)
     
-    
+def inputs():
+    inp = input()
+    print(inp)
+
 if __name__ == '__main__':
     pass
     cs()
